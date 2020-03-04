5c6a81240c64027480fc6c8ac10db27b1b38f662
Sam Gorick <samgorick.sg@gmail.com>
2020-03-04 20:34:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -14,6 +14,9 @@ def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
+introduction("Claire")
+introduction_with_language("Claire", "C#")
+introduction_with_language_optional("Claire")
 
 
 
